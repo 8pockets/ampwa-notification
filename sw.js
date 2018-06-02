da9e476d2c20655703a1aee5e7ccc7665860238e
8pockets <e.pockets@hotmail.co.jp>
2018-06-02 21:06:52 +0900

    first

---
+++ sw.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "helper-iframe.html",
+    "revision": "37afed71c4b45da33e1a7bb6850a4c7c"
+  },
+  {
+    "url": "index.html",
+    "revision": "68c02287db36604a96994b3633c569b1"
+  },
+  {
+    "url": "permission-dialog.html",
+    "revision": "a616f750edc862ea6b4c81c140f15585"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
